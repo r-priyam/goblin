a043b7c3b4f58b67fac760b313fc03e0191bd4b0
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-13 08:10:05 +0530

    chore: keep logging simple

--- src/goblin.ts
+++ src/goblin.ts
@@ -1,6 +1,5 @@
 import '#lib/setup';
 
-import { green } from 'colorette';
 import { GoblinClient } from '#lib/extensions/GoblinClient';
 import config from '#root/config';
 
@@ -8,7 +7,7 @@ const client = new GoblinClient();
 
 try {
 	await client.login(config.bot.token);
-	client.logger.info(`${green('WS     ')} - Successfully logged in.`);
+	client.logger.info('Successfully logged in.');
 } catch (error) {
 	client.logger.error(error);
 	client.destroy();
