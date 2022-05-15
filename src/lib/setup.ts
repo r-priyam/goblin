b4a70b6144ea85c400c1e49e78a6ef57e08244a6
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-15 13:41:15 +0530

    feat: setup postgres

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -3,11 +3,13 @@ import '@sapphire/plugin-logger/register';
 
 import { fileURLToPath } from 'node:url';
 import { inspect } from 'node:util';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { ApplicationCommandRegistries, Logger, container, Piece, RegisterBehavior } from '@sapphire/framework';
 import { Time } from '@sapphire/time-utilities';
 import { Client } from 'clashofclans.js';
-import { createColors } from 'colorette';
+import { blueBright, createColors, cyan, greenBright, redBright, yellow } from 'colorette';
 import dotenv from 'dotenv';
+import postgres, { Sql } from 'postgres';
 import type { GoblinClient } from './extensions/GoblinClient';
 import { srcFolder } from '#utils/constants';
 
@@ -20,18 +22,44 @@ ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior
 
 container.coc = new Client({ restRequestTimeout: Time.Second * 30 });
 
+const sqlHighlighter = new SqlHighlighter();
+
+container.sql = postgres({
+	debug(connection, query, parameters, types) {
+		container.logger.debug(
+			`${blueBright('Connections:')} ${yellow(connection)} » ${greenBright('Query:')} ${sqlHighlighter.highlight(query)} » ${redBright(
+				'Params:'
+			)} ${yellow(String(parameters || 'NULL'))} » ${cyan('Types:')} ${yellow(String(types || 'NULL'))}`
+		);
+	},
+	transform: {
+		column: { to: postgres.fromCamel, from: postgres.toCamel }
+	},
+	types: {
+		date: {
+			to: 1184,
+			from: [1082, 1083, 1114, 1184],
+			serialize: (date: Date) => date.toISOString(),
+			parse: (isoString) => isoString
+		}
+	}
+});
+
 Object.defineProperties(Piece.prototype, {
 	client: { get: () => container.client },
-	logger: { get: () => container.logger }
+	logger: { get: () => container.logger },
+	sql: { get: () => container.sql }
 });
 
 declare module '@sapphire/pieces' {
 	interface Container {
 		coc: Client;
+		sql: Sql<any>;
 	}
 	interface Piece {
 		client: GoblinClient;
 		logger: Logger;
 		coc: Client;
+		sql: Sql<any>;
 	}
 }
