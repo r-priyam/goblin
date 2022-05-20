202dec25adcffa72a90469d7e6ac2985d6025406
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-20 21:35:35 +0530

    feat(utils): add some interaction helpers

---
+++ src/lib/util/InteractionHelpers.ts
@@ -0,0 +1,22 @@
+import type { Message, MessageComponentInteraction } from 'discord.js';
+
+export async function verifyUser(interaction: MessageComponentInteraction, userId: string) {
+	console.log('called');
+	if (interaction.user.id !== userId) {
+		await interaction.followUp({
+			content: "These buttons can't be controlled by you, sorry!",
+			ephemeral: true
+		});
+		return false;
+	}
+
+	return true;
+}
+
+export async function waitForButtonClick(interactionMessage: Message, userId: string, timeout: number) {
+	return interactionMessage.awaitMessageComponent({
+		componentType: 'BUTTON',
+		time: timeout,
+		filter: async (interaction) => verifyUser(interaction, userId)
+	});
+}
