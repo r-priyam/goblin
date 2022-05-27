49f20203a15f70203a15b083d547b1201f8969d2
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-28 00:56:36 +0530

    chore: add github token in env var

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ const config = {
 		token: process.env.BOT_TOKEN,
 		owners: ['292332992251297794', '554882868091027456']
 	},
+	github: process.env.GITHUB_TOKEN,
 	clash: {
 		email: process.env.CLASH_EMAIL!,
 		password: process.env.CLASH_PASSWORD!,
