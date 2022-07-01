029d0d6943fca50f1a27dc78e4b5077a451832de
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-07-01 16:17:12 +0530

    feat(commands): add `supertroops` command

---
+++ src/commands/Clan/superTroops.ts
@@ -0,0 +1,98 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { ChatInputCommand } from '@sapphire/framework';
+import { isNullish, isNullishOrEmpty } from '@sapphire/utilities';
+import { Clan, SUPER_TROOPS } from 'clashofclans.js';
+import { MessageActionRow, MessageEmbed, MessageSelectMenu } from 'discord.js';
+
+import { clanHelper, SuperTroopEmotes } from '#lib/coc';
+import { GoblinCommand } from '#lib/extensions/GoblinCommand';
+import { Colors } from '#utils/constants';
+import { ClanOrPlayer, redis } from '#utils/redis';
+
+@ApplyOptions<ChatInputCommand.Options>({
+	description: 'Lists clan members active super troops'
+})
+export class SuperTroopsCommand extends GoblinCommand {
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder
+					.setName(this.name)
+					.setDescription(this.description)
+					.addStringOption((option) =>
+						option //
+							.setName('tag')
+							.setDescription('Tag of the clan')
+							.setRequired(false)
+							.setAutocomplete(true)
+					),
+			{
+				idHints: ['991987141259309067', '992380615884296213']
+			}
+		);
+	}
+
+	public override async chatInputRun(interaction: ChatInputCommand.Interaction<'cached'>) {
+		await interaction.deferReply();
+		let clanTag = interaction.options.getString('tag');
+
+		if (isNullish(clanTag)) {
+			const cachedClans = await redis.get<ClanOrPlayer[]>(`c-${interaction.user.id}`);
+			if (isNullish(cachedClans)) {
+				return interaction.editReply({
+					content: 'You have no clan linked into your profile. Please link any clan or provide the tag as 2nd argument!'
+				});
+			}
+
+			clanTag = cachedClans[0].tag;
+		}
+
+		const clan = await clanHelper.info(clanTag);
+		const embed = await SuperTroopsCommand.getSuperTroops(clan);
+		return interaction.editReply({ embeds: [embed], components: [this.menuOptions(clan.tag)] });
+	}
+
+	private menuOptions(clanTag: string) {
+		const superTroopsMenu = new MessageSelectMenu()
+			.setPlaceholder('Select a troop')
+			.setCustomId(`SUPER_TROOP_MENU_${clanTag}`)
+			.addOptions(SUPER_TROOPS.map((troop) => ({ label: troop, emoji: SuperTroopEmotes[troop], value: troop })));
+
+		return new MessageActionRow().addComponents(superTroopsMenu);
+	}
+
+	public static async getSuperTroops(clan: Clan, filterTroop?: string) {
+		const members = await clan.fetchMembers();
+		const superTroops = Object.fromEntries(SUPER_TROOPS.map((troop) => [`${SuperTroopEmotes[troop]} ${troop}`, [] as string[]]));
+
+		for (const member of members) {
+			for (const troop of member.superTroops) {
+				if (troop.isActive) superTroops[`${SuperTroopEmotes[troop.name]} ${troop.name}`].push(member.name);
+			}
+		}
+
+		const superTroopsEmbed = new MessageEmbed() //
+			.setColor(Colors.Indigo)
+			.setAuthor({ iconURL: clan.badge.url, name: clan.name })
+			.setTimestamp();
+
+		if (filterTroop) {
+			const data = superTroops[`${SuperTroopEmotes[filterTroop]} ${filterTroop}`];
+			if (isNullishOrEmpty(data)) {
+				superTroopsEmbed.description = `No one has boosted ${filterTroop} 🥲`;
+				return superTroopsEmbed;
+			}
+
+			superTroopsEmbed.addField(filterTroop, data.join('\n'), false);
+			return superTroopsEmbed;
+		}
+
+		for (const [troop, data] of Object.entries(superTroops)) {
+			if (isNullishOrEmpty(data)) continue;
+			superTroopsEmbed.addField(troop, data.join('\n'), false);
+		}
+
+		if (superTroopsEmbed.fields.length === 0) superTroopsEmbed.description = 'No one has boosted any super troop 🥲';
+		return superTroopsEmbed;
+	}
+}
---
+++ src/interaction-handlers/select-menus/superTroopMenu.ts
@@ -0,0 +1,27 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
+import type { SelectMenuInteraction } from 'discord.js';
+
+import { clanHelper } from '#lib/coc';
+import { SuperTroopsCommand } from '#root/commands/Clan/superTroops';
+
+@ApplyOptions<InteractionHandler.Options>({
+	interactionHandlerType: InteractionHandlerTypes.SelectMenu
+})
+export class SuperTroopMenu extends InteractionHandler {
+	public override async run(interaction: SelectMenuInteraction, result: InteractionHandler.ParseResult<this>) {
+		return interaction.followUp({ embeds: [result.data], ephemeral: true });
+	}
+
+	public override async parse(interaction: SelectMenuInteraction) {
+		if (!interaction.customId.startsWith('SUPER_TROOP_MENU')) return this.none();
+
+		await interaction.deferReply({ ephemeral: true });
+
+		const superTroop = interaction.values[0];
+
+		const clan = await clanHelper.info(interaction.customId.split('_').pop()!);
+		const data = await SuperTroopsCommand.getSuperTroops(clan, superTroop);
+		return this.some({ data });
+	}
+}
