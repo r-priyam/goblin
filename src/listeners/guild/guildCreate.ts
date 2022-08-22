2209bced2114fd2c722e025488b647102bdc3bbf
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-22 10:30:33 +0530

    refactor: guild listeners

--- src/listeners/guild/guildCreate.ts
+++ src/listeners/guild/guildCreate.ts
@@ -6,26 +6,27 @@ import { Colors } from '#utils/constants';
 import { useGuildLogsWebhook } from '#utils/webhooks/guildLogs';
 
 @ApplyOptions<Listener.Options>({
-	name: 'GuildLeave',
-	event: Events.GuildDelete
+	name: 'GuildCreate',
+	event: Events.GuildCreate
 })
-export class GuildDeleteListener extends Listener<typeof Events.GuildDelete> {
+export class BotListener extends Listener<typeof Events.GuildCreate> {
 	public override async run(guild: Guild) {
 		const webhook = useGuildLogsWebhook();
 
 		const owner = await guild.fetchOwner();
 		const guildDeleteEmbed = new MessageEmbed()
-			.setTitle('Left Guild')
+			.setTitle('Joined Guild')
 			.addFields([
 				{ name: 'Name', value: guild.name },
 				{ name: 'ID', value: guild.id },
 				{ name: 'Owner', value: `${owner.displayName} (ID: ${owner.id})` },
 				{ name: 'Total Members', value: guild.memberCount.toLocaleString() }
 			])
-			.setColor(Colors.Red)
+			.setColor(Colors.Green)
 			.setTimestamp();
 
 		if (guild.icon) guildDeleteEmbed.setThumbnail(guild.iconURL()!);
+		if (guild.me) guildDeleteEmbed.setTimestamp(guild.me.joinedTimestamp);
 
 		await webhook.send({ embeds: [guildDeleteEmbed] });
 	}
