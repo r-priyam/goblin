52f91ecfde03768527d5efd2a04c82aafa1cd96a
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-22 10:16:20 +0530

    chore: remove destructuring

--- src/listeners/errors/error.ts
+++ src/listeners/errors/error.ts
@@ -9,15 +9,14 @@ import { DiscordAPIError, HTTPError } from 'discord.js';
 })
 export class BotListener extends Listener<typeof Events.Error> {
 	public override run(error: Error) {
-		const { logger } = this.container;
 		if (error instanceof DiscordAPIError) {
-			logger.warn(this.errorSummary(error, 'API ERROR'));
-			logger.fatal(error.stack);
+			this.logger.warn(this.errorSummary(error, 'API ERROR'));
+			this.logger.fatal(error.stack);
 		} else if (error instanceof HTTPError) {
-			logger.warn(this.errorSummary(error, 'HTTP ERROR'));
-			logger.fatal(error.stack);
+			this.logger.warn(this.errorSummary(error, 'HTTP ERROR'));
+			this.logger.fatal(error.stack);
 		} else {
-			logger.error(error);
+			this.logger.error(error);
 		}
 	}
 
