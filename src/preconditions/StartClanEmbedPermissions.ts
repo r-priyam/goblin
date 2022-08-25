468b3aa145fe92a637106bb4e2c9d23c45e7c1f1
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-25 12:53:32 +0530

    feat: add `preCondition` for start command check

---
+++ src/preconditions/StartClanEmbedPermissions.ts
@@ -0,0 +1,42 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Precondition } from '@sapphire/framework';
+import { PermissionFlagsBits } from 'discord-api-types/v10';
+import { CommandInteraction } from 'discord.js';
+
+import { Emotes } from '#utils/constants';
+
+@ApplyOptions<Precondition.Options>({
+	name: 'StartRequiredPermissions'
+})
+export class UserPrecondition extends Precondition {
+	#requiredPermissions = [
+		PermissionFlagsBits.ViewChannel,
+		PermissionFlagsBits.ReadMessageHistory,
+		PermissionFlagsBits.SendMessages,
+		PermissionFlagsBits.ManageMessages,
+		PermissionFlagsBits.UseExternalEmojis,
+		PermissionFlagsBits.EmbedLinks
+	];
+
+	public override async chatInputRun(interaction: CommandInteraction) {
+		const missingPerms = interaction.appPermissions?.missing(this.#requiredPermissions);
+
+		return missingPerms ? this.error({ message: this.formatMissingPerms(missingPerms) }) : this.ok();
+	}
+
+	private formatMissingPerms(perms: string[]) {
+		const noun = perms.length === 1 ? 'permission' : 'permissions';
+		let formatted = `I am missing some ${noun} to execute this command. Please allow the ${noun} mentioned below to me:\n`;
+		for (const perm of perms)
+			formatted += `$${Emotes.Error}${perm
+				.replace('_', ' ')
+				.replace(/\w\S*/g, (replace) => replace.charAt(0).toUpperCase() + replace.slice(1).toLowerCase())}\n`;
+		return formatted.slice(0, -2);
+	}
+}
+
+declare module '@sapphire/framework' {
+	interface Preconditions {
+		StartRequiredPermissions: never;
+	}
+}
