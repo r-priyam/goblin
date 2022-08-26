5649b19d27c106c182d1dac09a0a5de5681490c0
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-27 00:44:40 +0530

    refactor: set current requirement value

--- src/interaction-handlers/buttons/requirementButton.ts
+++ src/interaction-handlers/buttons/requirementButton.ts
@@ -12,7 +12,7 @@ export class ButtonHandler extends InteractionHandler {
 		return interaction.showModal(result.modal);
 	}
 
-	public override parse(interaction: ButtonInteraction) {
+	public override async parse(interaction: ButtonInteraction) {
 		if (
 			!interaction.customId.startsWith(ButtonCustomIds.AddClanRequirement) &&
 			!interaction.customId.startsWith(ButtonCustomIds.UpdateClanRequirement)
@@ -22,38 +22,43 @@ export class ButtonHandler extends InteractionHandler {
 
 		const clanTag = interaction.customId.split('-').pop()!;
 		const updateRequirements = interaction.customId === ButtonCustomIds.UpdateClanRequirement;
+		const [currentRequirements] = await this.sql<[{ requirements: Record<string, number> }]>`SELECT requirements
+                                                                                                 FROM clan_embeds`;
 
-		return this.some({ modal: this.requirementsModel(clanTag, updateRequirements) });
+		return this.some({ modal: this.requirementsModel(clanTag, updateRequirements, currentRequirements.requirements) });
 	}
 
-	private requirementsModel(clanTag: string, updateRequirements: boolean) {
+	private requirementsModel(clanTag: string, updateRequirements: boolean, requirements: Record<string, number>) {
 		return new Modal()
 			.setTitle(`${updateRequirements ? 'Update' : 'Add'} Clan Requirements Form`)
 			.setCustomId(`${updateRequirements ? ModalCustomIds.UpdateClanEmbedRequirements : ModalCustomIds.AddClanEmbedRequirements}-${clanTag}`)
 			.addComponents(
 				new MessageActionRow<ModalActionRowComponent>().addComponents(
-					this.requirementsModelInput(14, ModalInputCustomIds.FourteenRequirements)
+					this.requirementsModelInput(14, ModalInputCustomIds.FourteenRequirements, String(requirements['14']))
 				),
 				new MessageActionRow<ModalActionRowComponent>().addComponents(
-					this.requirementsModelInput(13, ModalInputCustomIds.ThirteenRequirements)
+					this.requirementsModelInput(13, ModalInputCustomIds.ThirteenRequirements, String(requirements['13']))
 				),
 				new MessageActionRow<ModalActionRowComponent>().addComponents(
-					this.requirementsModelInput(12, ModalInputCustomIds.TwelveRequirements)
+					this.requirementsModelInput(12, ModalInputCustomIds.TwelveRequirements, String(requirements['12']))
 				),
 				new MessageActionRow<ModalActionRowComponent>().addComponents(
-					this.requirementsModelInput(11, ModalInputCustomIds.ElevenRequirements)
+					this.requirementsModelInput(11, ModalInputCustomIds.ElevenRequirements, String(requirements['11']))
 				),
-				new MessageActionRow<ModalActionRowComponent>().addComponents(this.requirementsModelInput(10, ModalInputCustomIds.TenRequirements))
+				new MessageActionRow<ModalActionRowComponent>().addComponents(
+					this.requirementsModelInput(10, ModalInputCustomIds.TenRequirements, String(requirements['10']))
+				)
 			);
 	}
 
-	private requirementsModelInput(townHallLevel: number, customId: string) {
+	private requirementsModelInput(townHallLevel: number, customId: string, value: string) {
 		return new TextInputComponent()
 			.setCustomId(customId)
 			.setLabel(`How many ${townHallLevel} you need?`)
 			.setStyle('SHORT')
 			.setMinLength(1)
 			.setMaxLength(2)
+			.setValue(value)
 			.setRequired(true);
 	}
 }
