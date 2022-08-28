3f99804173efbe2bdaf88a0bd0b46dd11ad80e1e
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-28 14:11:36 +0530

    fix: broken cron string

--- src/scheduled-tasks/syncClanEmbed.ts
+++ src/scheduled-tasks/syncClanEmbed.ts
@@ -8,7 +8,7 @@ import { Constants, HTTPError } from 'discord.js';
 import { BlueNumberEmotes, ClanHelper, TownHallEmotes } from '#lib/coc';
 
 @ApplyOptions<ScheduledTask.Options>({
-	cron: '* */2 * * *'
+	cron: '0 */2 * * *'
 })
 export class SyncClanEmbedTask extends ScheduledTask {
 	public override async run() {
