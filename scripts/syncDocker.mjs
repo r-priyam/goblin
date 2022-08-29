0e3fbd568dc08b3e5f4176983fc9794518a4c49e
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-08-29 12:40:35 +0530

    feat: add script to perform bot image refresh

---
+++ scripts/syncDocker.mjs
@@ -0,0 +1,26 @@
+import { execSync } from 'node:child_process';
+import { exit } from 'node:process';
+import { Result } from '@sapphire/framework';
+import { cyanBright, greenBright, redBright } from 'colorette';
+
+function executeCommand(operation, action) {
+	process.stdout.write(cyanBright(`⏳ Running ${operation} `));
+
+	const result = Result.from(() => {
+		action();
+		console.log(greenBright('✅ Done'));
+	});
+
+	if (result.isErr()) {
+		console.log(redBright('❌ Error'));
+		console.error(result.unwrapErr().message);
+		exit(1);
+	}
+
+	return result.unwrap();
+}
+executeCommand('Git pull', () => execSync('git pull'));
+executeCommand('Stop bot service', () => execSync('docker compose -f .docker/docker-compose.yml stop bot'));
+executeCommand('Pull bot latest image', () => execSync('docker-compose -f .docker/docker-compose.yml pull bot'));
+executeCommand('Start bot', () => execSync('docker-compose up -f .docker/docker-compose.yml -d bot'));
+exit(0);
