54bebfcb2072a8c582960ebe583b9c76b3876629
r-priyam <roy_priyam@outlook.com>
2022-09-25 10:35:52 +0530

    refactor: move code to client [skip cd]

--- src/lib/extensions/GoblinClient.ts
+++ src/lib/extensions/GoblinClient.ts
@@ -1,7 +1,11 @@
+import { REST } from '@discordjs/rest';
 import { container, LogLevel, SapphireClient } from '@sapphire/framework';
 import { ScheduledTaskRedisStrategy } from '@sapphire/plugin-scheduled-tasks/register-redis';
 import { envParseBoolean, envParseInteger, envParseString } from '@skyra/env-utilities';
 import { GatewayIntentBits } from 'discord-api-types/v10';
+import postgres from 'postgres';
+import { GoblinClashClient } from '#lib/coc';
+import { GoblinRedisClient } from '#lib/redis-cache/RedisCacheClient';
 import { logSuccess } from '#utils/functions/logging';
 
 export class GoblinClient extends SapphireClient {
@@ -28,6 +32,24 @@ export class GoblinClient extends SapphireClient {
 						}
 					}
 				})
+			}
+		});
+
+		// Inject things in container
+		container.coc = new GoblinClashClient();
+		container.discordRest = new REST({ version: '10' }).setToken(envParseString('DISCORD_TOKEN'));
+		container.redis = new GoblinRedisClient();
+		container.sql = postgres({
+			transform: {
+				column: { to: postgres.fromCamel, from: postgres.toCamel }
+			},
+			types: {
+				date: {
+					to: 1184,
+					from: [1082, 1083, 1114, 1184],
+					serialize: (date: Date) => date.toISOString(),
+					parse: (isoString) => isoString
+				}
 			}
 		});
 	}
--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,14 +1,12 @@
 import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
+
 import process from 'node:process';
 import { URL } from 'node:url';
 import { inspect } from 'node:util';
-import { REST } from '@discordjs/rest';
 import { container, Piece } from '@sapphire/framework';
-import { envParseString, setup } from '@skyra/env-utilities';
+import { setup } from '@skyra/env-utilities';
 import { createColors } from 'colorette';
-import postgres from 'postgres';
-import { GoblinClashClient } from '#lib/coc';
 import { GoblinRedisClient } from '#lib/redis-cache/RedisCacheClient';
 import { SrcDir } from '#utils/constants';
 import { logSuccess, logWarning } from '#utils/functions/logging';
@@ -24,24 +22,7 @@ container.redis.on('ready', () => container.logger.info(logSuccess('REDIS', 'Con
 container.redis.on('error', (error) => container.logger.error(error));
 container.redis.on('reconnecting', () => container.logger.warn(logWarning('REDIS', 'Reconnecting')));
 
-container.coc = new GoblinClashClient();
-// TODO: remove in djs v14, it's exposed
-container.discordRest = new REST({ version: '10' }).setToken(envParseString('DISCORD_TOKEN'));
-
-container.sql = postgres({
-	transform: {
-		column: { to: postgres.fromCamel, from: postgres.toCamel }
-	},
-	types: {
-		date: {
-			to: 1184,
-			from: [1082, 1083, 1114, 1184],
-			serialize: (date: Date) => date.toISOString(),
-			parse: (isoString) => isoString
-		}
-	}
-});
-
+// Make things easily accessible by this.<> instead of this.container.<>
 Object.defineProperties(Piece.prototype, {
 	client: { get: () => container.client },
 	logger: { get: () => container.logger },
